05a13f6b4d9bfc299592529b4f47644737fd7d01
Eric Collins <eric@tabfugni.cc>
2017-08-04 18:37:43 -0400

    Convert to const instead of var

--- unkeybound.js
+++ unkeybound.js
@@ -1,4 +1,4 @@
-var toolbarItems = document.
+const toolbarItems = document.
     querySelectorAll(".js-toolbar-item[data-toolbar-hotkey='k']");
 
 toolbarItems.forEach(function(item) { item.remove(); });
